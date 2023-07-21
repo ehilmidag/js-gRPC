b554cbd32323be96f7a400c4080121989efed0a8
hilmi <hilmidag@Hilmi-MacBook-Pro.local>
2023-07-22 02:29:38 +0300

    service side added

---
+++ srv.js
@@ -0,0 +1,25 @@
+const grpc = require("grpc");
+const protoLoad = require("@grpc/proto-loader")
+
+const packageDefination = protoLoad.loadSync("todolist.proto",{});
+const grpcObj = grpc.loadPackageDefinition(packageDefination);
+
+const todoListPackage = grpcObj.todoListPackage;
+
+const server = new grpc.Server();
+server.bind("127.0.0.1:40000", grpc.ServerCredentials.createInsecure());
+
+server.addService(todoListPackage.TodoList.service, {
+"createTodoElement":createTodoElement,
+"readTodoList":readTodoList
+});
+
+server.start();
+
+function createTodoElement(call,callback) {
+console.log(call)
+}
+
+function readTodoList(call,callback) {
+
+}
